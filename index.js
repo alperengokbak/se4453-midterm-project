4f5e7e2722bfa3b590b6dd35992ff50bd44026c3
alperengokbak <swalperen3008@gmail.com>
2024-04-01 21:01:54 +0300

    Feature: Configured 'cors'.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Import express
 import express from "express";
+import cors from "cors";
 
 // Import routes
 import router from "./src/router/helloRouter.js";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.use("/hello", router);
 
