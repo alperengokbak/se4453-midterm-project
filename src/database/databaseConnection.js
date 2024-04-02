c0136f5b86deb27ae5d05daa7b133f8468b1168d
alperengokbak <swalperen3008@gmail.com>
2024-04-03 01:44:11 +0300

    r

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -1,10 +1,15 @@
 import pkg from "pg";
 import dotenv from "dotenv";
 import fs from "fs";
+import path from "path";
 
 dotenv.config();
 
 const { Client } = pkg;
+
+const currentDir = path.dirname(new URL(import.meta.url).pathname);
+
+const sslCertPath = path.resolve(currentDir, process.env.PGCERT);
 
 export const conn = new Client({
   host: process.env.PGHOST,
@@ -12,4 +17,8 @@ export const conn = new Client({
   user: process.env.PGUSER,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
+  ssl: {
+    ca: fs.readFileSync(sslCertPath),
+    rejectUnauthorized: true,
+  },
 });
