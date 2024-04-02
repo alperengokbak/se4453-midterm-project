863bc32c8a05cacb165298f68206cc14af1dcca8
alperengokbak <swalperen3008@gmail.com>
2024-04-03 02:17:36 +0300

    Refactoring: Updated database connection.

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -1,15 +1,9 @@
 import pkg from "pg";
 import dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
 
 dotenv.config();
 
 const { Client } = pkg;
-
-const currentDir = path.dirname(new URL(import.meta.url).pathname);
-
-const sslCertPath = path.resolve(currentDir, process.env.PGCERT);
 
 export const conn = new Client({
   host: process.env.PGHOST,
@@ -17,8 +11,4 @@ export const conn = new Client({
   user: process.env.PGUSER,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  ssl: {
-    ca: fs.readFileSync(sslCertPath),
-    rejectUnauthorized: true,
-  },
 });
